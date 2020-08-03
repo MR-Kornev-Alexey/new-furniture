87f0f2d979587857731bdfd456ad2e9f85f195db
Ваше Имя <you@example.com>
2020-08-03 21:21:21 +0200

    first commit

--- src/components/v-nav-bar/v-nav-bar.vue
+++ src/components/v-nav-bar/v-nav-bar.vue
@@ -10,23 +10,52 @@
     >
       <v-app-bar-nav-icon @click="drawer = true" v-if="$vuetify.breakpoint.smOnly"></v-app-bar-nav-icon>
       <v-row class="d-flex justify-center">
-        <v-col md="1">
+        <v-col class="d-flex justify-end pr-2" md="3">
           <img alt="logo" src="../../assets/img/svg/logo.svg">
         </v-col>
-        <v-col md="10">
+        <v-col class="main-nav" md="9">
           <div class="d-flex">
-            <div :key="i"
-                             v-for="(item, i) in mainMenu">
+            <div :class="[
+              { 'margin-left-0': item.left0 },
+              { 'margin-left-50': item.left50 },
+              { 'margin-left-42': item.left42}
+            ]"
+                 :key="i"
+                 v-for="(item, i) in mainMenu">
               {{item.name}}
             </div>
-            <img alt="Search" src="../../assets/img/svg/search.svg" style="height: 17px">
-            <img alt="Search" src="../../assets/img/svg/shopping-bag.svg" style="height: 17px">
+            <div class="nav-icons d-flex">
+              <div>
+                <img alt="Search" src="../../assets/img/svg/search.svg" style="height: 17px">
+              </div>
+              <div class="shopping-bag">
+                <img  alt="bag" src="../../assets/img/svg/shopping-bag.svg" style="height: 17px">
+                <div class="shopping-bag__round d-flex justify-center align-content-center">
+                  <div class="align-self-center">1</div>
+                </div>
+              </div>
+
+            </div>
+
           </div>
-          <div class="d-flex">
-            <div :key="i"
-                             v-for="(item, i) in subMenu">
-              {{item.name}}
-            </div>
+
+          <div class="d-flex sub-nav ">
+            <div class="d-flex sub-nav__menu">
+              <div :key="i"
+                   v-for="(item, i) in subMenu"
+                   :class="[
+              { 'margin-left-0': item.left0 },
+              { 'margin-left-50': item.left50 },
+              { 'margin-left-62': item.left62}
+            ]"
+              >
+                {{item.name}}
+              </div>
+             </div>
+           <div class="d-block nav-phone justify-end">
+            <div class=" text-right">+7 (495) 792-0668</div>
+             <div class="nav-phone__sub">Современная мебельная фабрика</div>
+           </div>
           </div>
 
         </v-col>
@@ -81,55 +110,55 @@ export default {
     drawer: false,
     mainMenu: [
       {
-        id: 0,
+        left0: true,
         name: 'Загрузка проекта',
         dropdown: false
       },
       {
-        id: 1,
+        left50: true,
         name: 'Доставка и оплата',
         dropdown: false
       },
       {
-        id: 2,
+        left42: true,
         name: 'О производстве',
         dropdown: true
       },
       {
-        id: 3,
+        left42: true,
         name: 'Блог',
         dropdown: true
       },
       {
-        id: 4,
+        left42: true,
         name: 'Портфолио',
         dropdown: true
       },
       {
-        id: 5,
+        left50: true,
         name: 'Контакты',
         dropdown: true
       }
     ],
     subMenu: [
       {
-        id: 0,
+        left0: true,
         name: 'Диваны'
       },
       {
-        id: 1,
+        left62: true,
         name: 'Кровати'
       },
       {
-        id: 2,
+        left50: true,
         name: 'Детская мебель'
       },
       {
-        id: 3,
+        left50: true,
         name: 'Кресла'
       },
       {
-        id: 4,
+        left50: true,
         name: 'Тумбы'
       }
     ]
@@ -137,6 +166,56 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+  .main-nav {
+    padding-left: 36px;
+  }
+  .sub-nav {
+    margin-top: 34px;
+
+    .sub-nav__menu{
+      margin-top: 10px;
+      text-transform: uppercase;
+    }
+  }
+  .margin-left-0{
+    margin-left: 0;
+  }
+  .margin-left-50{
+    margin-left: 50px;
+  }
+  .margin-left-42{
+    margin-left: 50px;
+  }
+  .margin-left-62{
+    margin-left: 62px;
+  }
+  .nav-icons{
+    margin-left: 196px;
+    .shopping-bag{
+      margin-left: 40px;
+      position: relative;
+      .shopping-bag__round{
+         position: absolute;
+        bottom: -4px;
+        right: -4px;
+        background-color:  #FDC94C;
+        width: 13px;
+        height: 13px;
+        border-radius: 50%;
+        div{
+          font-size: 11px;
+          }
+      }
+    }
+
+  }
+  .nav-phone{
+    margin-left: 290px;
+    .nav-phone__sub{
+      font-size: 12px;
+      line-height: 14px;
+    }
+  }
 </style>
