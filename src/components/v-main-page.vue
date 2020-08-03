5be42b8c8f5ef59ce49f56bbbbf065dab7859e12
Ваше Имя <you@example.com>
2020-08-03 18:09:25 +0200

    first commit

---
+++ src/components/v-main-page.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container class="v-main-page">
+    <v-nav-bar/>
+    <v-row class="text-center">
+111
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import vNavBar from '../components/v-nav-bar/v-nav-bar'
+export default {
+  name: 'v-main-page',
+  components: {
+    vNavBar
+  },
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader'
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify'
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+      }
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com'
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com'
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify'
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs'
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify'
+      }
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer'
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts'
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+      }
+    ]
+  })
+}
+</script>
---
+++ src/components/v-nav-bar/v-nav-bar.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-card
+    class="mx-auto overflow-hidden"
+    height="100vh"
+  >
+    <v-app-bar
+      color="white"
+      light
+
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title v-for="(item, i) in mainMenu"
+                       :key="i">
+        {{item.name}}
+      </v-toolbar-title>
+      <div >
+        123
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'v-nav-bar',
+  data: () => ({
+    group: '',
+    drawer: false,
+    mainMenu: [
+      {
+        id: 0,
+        name: 'Загрузка проекта',
+        dropdown: false
+      },
+      {
+        id: 1,
+        name: 'Доставка и оплата',
+        dropdown: false
+      },
+      {
+        id: 2,
+        name: 'О производстве',
+        dropdown: true
+      },
+      {
+        id: 3,
+        name: 'Блог',
+        dropdown: true
+      },
+      {
+        id: 4,
+        name: 'Портфолио',
+        dropdown: true
+      },
+      {
+        id: 5,
+        name: 'Контакты',
+        dropdown: true
+      }
+    ],
+    subMenu: [
+      {
+        id: 0,
+        name: 'Диваны'
+      },
+      {
+        id: 1,
+        name: 'Кровати'
+      },
+      {
+        id: 2,
+        name: 'Детская мебель'
+      },
+      {
+        id: 3,
+        name: 'Кресла'
+      },
+      {
+        id: 4,
+        name: 'Тумбы'
+      }
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
