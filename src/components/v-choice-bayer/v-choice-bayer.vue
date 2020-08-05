25419db0e0ef74e36cdc45bd2c0f939e1c895a13
Ваше Имя <you@example.com>
2020-08-05 11:35:06 +0200

    first commit

--- src/components/v-choice-bayer/v-choice-bayer.vue
+++ src/components/v-choice-bayer/v-choice-bayer.vue
@@ -1,23 +1,46 @@
 <template>
-  <v-row class="v-choice-bayer d-block">
+  <div class="v-choice-bayer">
     <h2>Выбор покупателей</h2><br>
-    <h3>Здесь будет карусель</h3>
-   </v-row>
+    <carousel :per-page="4" :mouse-drag="true">
+      <slide>
+        Slide 1 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+      <slide>
+        Slide 3 Content
+      </slide>
+      <slide>
+        Slide 4 Content
+      </slide>
+      <slide>
+        Slide 5 Content
+      </slide>
+    </carousel>
+  </div>
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel'
+
 export default {
-  name: 'v-choice-bayer'
+  name: 'v-choice-bayer',
+  components: {
+    Carousel,
+    Slide
+  }
 }
 </script>
 
-<style  lang="scss" scoped>
-  .v-choice-bayer{
+<style lang="scss" scoped>
+  .v-choice-bayer {
     margin: 122px auto 0 auto;
     width: 1288px;
     height: 510px;
     background-color: aliceblue;
-    h2{
+
+    h2 {
       font-size: 30px;
       line-height: 30px;
     }
