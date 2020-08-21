234e24f197827d1b184d8134833d926201b68768
Ваше Имя <you@example.com>
2020-08-21 21:28:19 +0200

    12 08 all ready

--- src/components/v-main-fabric/v-main-fabric.vue
+++ src/components/v-main-fabric/v-main-fabric.vue
@@ -19,16 +19,54 @@
          <div>
            {{item.name}}
          </div>
-          <v-icon v-if="item.active" class="v-icon-small">mdi-chevron-down</v-icon>
+          <v-icon v-if="item.active" class="v-icon-small " >mdi-chevron-down</v-icon>
         </div>
+      </div>
+      <div class="row d-flex justify-end">
+        <div class="col-md-3 col-left">
+         <div class="v-main-fabric-tittle">
+           ВЕЛЮР
+         </div>
+          <div class="v-main-fabric-description">
+            <p>
+              Густой бархатистый ворс, мягкий и приятный на ощупь.
+            </p>
+            <p>
+              Плотность варьируется от 240 до 375 г/м².
+              Эта цифра указывает вес нитей в граммах на квадратный метр материала.
+              Чем плотнее ткань, тем больше значение. Для сравнения: средняя плотность имеет
+              показатель 200 г/м².
+            </p>
+            <p>
+              Устойчивость к истиранию по тесту Мартиндейла от 20 000 до 40 000 циклов.
+            </p>
+          </div>
+          <v-transit  :text = 'textOut2'
+                      :address = 'addressOut'
+                      :color= 'colorBlack'
+                      class="corp-transit"
+          />
+        </div>
+        <div class="col-md-7 v-main-fabric-pic">
+          <img src="../../assets/img/main-product/vabric-velur-new.png" alt="velur">
+        </div>
+
       </div>
     </div>
 </template>
 
 <script>
+import vTransit from '../v-common/v-transit'
+
 export default {
   name: 'v-main-fabric',
+  components: {
+    vTransit
+  },
   data: () => ({
+    textOut2: 'Выбрать цвет',
+    colorBlack: true,
+    addressOut: '#',
     mainFabric: [
       {
         name: 'Шенил',
@@ -68,6 +106,39 @@ export default {
 <style lang="scss">
   .v-main-fabric{
     margin: 80px auto 0 auto ;
+    .col-left{
+      margin: 115px -79px 0 0;
+      .corp-transit{
+        margin-top: 19px;
+      }
+    }
+    .v-main-fabric-tittle{
+      font-size: 30px;
+      line-height: 30px;
+      color: #000000;
+    }
+    .v-main-fabric-description{
+      margin: 25px 0 0 0;
+      font-size: 14px;
+      line-height: 136.5%;
+      /* or 19px */
+      color: #000000;
+      max-width: 320px;
+      min-height: 220px;
+    }
+   .v-main-fabric-pic{
+     position: relative;
+      margin: 66px 13px 0 0;
+      background: url("../../assets/img/main-product/sofa-velur.png") no-repeat;
+      background-size: cover;
+      width: 1133px;
+      height: 502px;
+     img{
+       position: absolute;
+       top: -66px;
+       left: 60px;
+     }
+    }
     .row-fabric{
       margin: 41px 0 0 0;
       .item-fabric{
